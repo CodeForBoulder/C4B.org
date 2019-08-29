d4fe1c282ef8961a5f37f949289daf2df6517017
Su-ryun <dlee67@msudenver.edu>
2019-08-28 21:35:28 -0600

    Not sure why Sass is not communicating with the .jsx.

--- c4b/src/header/Header.jsx
+++ c4b/src/header/Header.jsx
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 
 class Header extends Component {
 
+  // Literal copy pasta of the below:
+  // https://www.w3schools.com/bootstrap/bootstrap_navbar.asp
   render() {
     return( 
-      <div className="cfb-header">
-        <img src="http://static1.squarespace.com/static/5ab1bc5412b13f4dc838bc43/t/5ab1c7c1562fa7620d315119/1565828100449/" width="50" height="50"/>
-        <a href="/home"> home </a>
-        <a href="/new-events"> New Events </a>
-        <a href="/who-we-are"> What We Do </a>
-        <a href="/work-with-us"> Work With Us </a>
-        <a href="https://secure.codeforamerica.org/page/contribute/default?source_codes=Brigade-page&brigade=Code%20for%20Boulder"> Donate </a>
-        <a href="/get-involved"> Get Involved </a>
-      </div>
+      <nav class="navbar navbar-default">
+        <div class="navbar-header">
+          <img className="cb-logo" src="http://static1.squarespace.com/static/5ab1bc5412b13f4dc838bc43/t/5ab1c7c1562fa7620d315119/1565828100449/" width="50" height="50"/>
+        </div>
+        <ul id="header-right" className="nav navbar-nav navbar-right">
+          <li><a href="/home"> home </a> </li>
+          <li><a href="/new-events"> New Events </a> </li>
+          <li><a href="/who-we-are"> What We Do </a> </li>
+          <li><a href="/work-with-us"> Work With Us </a> </li>
+          <li><a href="https://secure.codeforamerica.org/page/contribute/default?source_codes=Brigade-page&brigade=Code%20for%20Boulder"> Donate </a> </li>
+          <li><a href="/get-involved"> Get Involved </a> </li>
+        </ul>
+      </nav>
     )
   }
   
