70951f3a3b6d73bfab37c439e51bd945d6da57e0
Su-ryun <dlee67@msudenver.edu>
2019-09-02 02:00:04 -0600

    Made more progress, but I am not sure where I can get the pictures/icons to really complete the app.

--- c4b/src/footer/Footer.jsx
+++ c4b/src/footer/Footer.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import './Footer.css';
 
 class Footer extends Component {
 
   render() {
     return( 
-      <div>
-        <p> Lol, footer.</p>
-      </div>
+      <footer class="page-footer font-small cyan darken-3">
+        <ul class="list-unstyled list-inline text-center">
+          <li class="list-inline-item">
+            Footer goes here.
+          </li>
+        </ul>
+        <div class="footer-copyright text-center py-3">
+          Copyright (c) 2018, Code For Boulder
+        </div>
+      </footer>
     )
   }
   
