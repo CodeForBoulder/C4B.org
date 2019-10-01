4e7d7c5b6104dd0c8808f20731c93a57882454f5
Su-ryun <dlee67@msudenver.edu>
2019-10-01 00:47:54 -0600

    Getting started on open data sources page.

--- c4b/src/pages/openDataSources/OpenDataSources.jsx
+++ c4b/src/pages/openDataSources/OpenDataSources.jsx
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import './OpenDataSources.css'
 
 class OpenDataSources extends Component {
 
     render() {
         return(
-            <div>
-                Art Of War
+            <div className="container">
+                <div id="open-data-sources" className="row">
+                    <h4 id="open-data-sources-header"> OPEN DATA SOURCES</h4>
+                    <p id="open-data-sources-para"> 
+                        Open data plays a crucial part in Code for Boulder. 
+                        Without open data, there would be no open projects! 
+                        Here are a few sources of data curated by municipalities and organizations both in Boulder and across the state.
+                    </p>    
+                </div>
             </div>
         )
     }
